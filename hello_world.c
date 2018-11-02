6343c1d1b4f430d58cd0d354ad240c7d23fbf92e
codewalker87 <codewalker_87@outlook.com>
2018-11-02 16:57:18 +0530

    Hello world with Date and Time

--- hello_world.c
+++ hello_world.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+#include <time.h>
 int main()
 {
-	printf("Hello world\n");
+	time_t t = time(NULL);
+	struct tm tm = *localtime(&t);
+	system("clear");
+	puts("Hello world");
+	printf("Date: %d/%d/%d\n", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900);
+	printf("Time: %d:%d:%d\n", tm.tm_hour, tm.tm_min, tm.tm_sec);
 }

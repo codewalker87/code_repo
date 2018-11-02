5f7ad21da99a56553c557ff9289b34531471c599
codewalker87 <codewalker_87@outlook.com>
2018-11-02 22:29:41 +0530

    Major update: This patch adds the date and time before the "Hello world" line
    Signed-off-by: codewalker87 <codewalker_87@outlook.com>

--- hello_world.c
+++ hello_world.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <time.h>
 int main()
 {
 	char name[50];
+	time_t t = time(NULL);
+	struct tm tm = *localtime(&t);
+	
+	printf("Date: %d/%d/%d\n", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900);
+	printf("Time: %d:%d:%d\n", tm.tm_hour, tm.tm_min, tm.tm_sec);
+	
 	printf("Hello world\n");
 	printf("Enter your name: ");
 	scanf("%[^\n]s", name);
